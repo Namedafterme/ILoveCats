f76081ba9a56b3d2763eeee0fe1d78b90ad445be
Namedafterme <younamedafterme@gmail.com>
2021-06-29 17:49:12 +0900

    게임씬 완료
    전체 스토리는 완료,
    게임 종료  버튼 연동 필요
    오프닝 화면 영상만 넣자!

---
+++ Assets/2. Script/GetAllRewards.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetAllRewards : MonoBehaviour
+{
+    public GameObject CactusWren;
+    public GameObject Rat;
+    public GameObject Rabbit;
+
+    public GameObject Ending;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
--- Assets/2. Script/HumanCtrl.cs
+++ Assets/2. Script/HumanCtrl.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HumanCtrl : MonoBehaviour
 {
     public float speed = 1f;
+
+    public Rigidbody RB;
+
+    public GameObject CanvusCat1;
+    public GameObject CanvusCat2;
+    public GameObject CanvusCat3;
+    public GameObject CanvusTiger;
+
+    public GameObject EndingCanvus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -31,6 +42,39 @@ public class HumanCtrl : MonoBehaviour
             transform.position += Vector3.back * speed * Time.deltaTime;
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RB.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+        }
+
+        // 3가지 보상을 얻었을 때 EndingCanvus가 켜지게 하려면?
+        // 보상에는 REWARD 태그가 들어있음
+       
+ 
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "CAT1")
+        {
+            CanvusCat1.SetActive(true);
+        }
+
+        if (collision.transform.tag == "CAT2")
+        {
+            CanvusCat2.SetActive(true);
+        }
+
+        if (collision.transform.tag == "CAT3")
+        {
+            CanvusCat3.SetActive(true);
+        }
+
+        if (collision.transform.tag == "CAT")
+        {
+            CanvusTiger.SetActive(true);
+        }
     }
 
 }
